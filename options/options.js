21ec7f4d51c68e5f607d537987a72ca26745ab03
Crowdbotics <engineering@crowdbotics.com>
2023-06-19 06:08:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fragrant-silence-de-92798",
+  url: "https://fragrant-silence-de-92798.botics.co",
+  api: "https://fragrant-silence-de-92798.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
